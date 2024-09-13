c9e62ecaf6eeba1a14b8e11b37692f236a2b308b
AChirkova <leadenrain@yandex.ru>
2024-09-13 16:04:04 +0500

    add closeHook

---
+++ src/components/article-params-form/hooks/useClose.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+
+type UseClose = {
+	isOpen: boolean;
+	onChange: (newValue: boolean) => void;
+	onClose?: () => void;
+	rootRef: React.RefObject<HTMLDivElement>;
+};
+
+export const useClose = ({ isOpen, rootRef, onClose, onChange }: UseClose) => {
+	useEffect(() => {
+		const handleOutsideClickClose = (evt: MouseEvent) => {
+			const { target } = evt;
+			if (target instanceof Node && !rootRef.current?.contains(target)) {
+				isOpen && onClose?.();
+				onChange?.(false);
+			}
+		};
+
+		const handleCloseByEsc = (evt: KeyboardEvent) => {
+			if (evt.key === 'Escape') {
+				onChange?.(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleOutsideClickClose);
+		document.addEventListener('keydown', handleCloseByEsc);
+
+		return () => {
+			document.removeEventListener('mousedown', handleOutsideClickClose);
+			document.removeEventListener('keydown', handleCloseByEsc);
+		};
+	}, [onClose, onChange, isOpen]);
+};
