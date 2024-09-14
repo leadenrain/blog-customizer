655f913912276dc406909bea76bdd6b206849bae
AChirkova <leadenrain@yandex.ru>
2024-09-14 19:00:28 +0500

    app moved

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,17 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
-import clsx from 'clsx';
-
-import { Article } from './components/article/Article';
-import {
-	ArticleStateType,
-	defaultArticleState,
-} from './constants/articleProps';
+import { StrictMode } from 'react';
 
 import './styles/index.scss';
-import styles from './styles/index.module.scss';
-import { ArticleParamsForm } from './components/article-params-form';
+import { App } from './components/App/App';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
-const App = () => {
-	const [articleState, setArticleState] =
-		useState<ArticleStateType>(defaultArticleState);
-
-	return (
-		<div
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': articleState.fontFamilyOption.value,
-					'--font-size': articleState.fontSizeOption.value,
-					'--font-color': articleState.fontColorOption.value,
-					'--container-width': articleState.contentWidthArrOption.value,
-					'--bg-color': articleState.backgroundColorOption.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm changeStyles={setArticleState} />
-			<Article />
-		</div>
-	);
-};
-
 root.render(
 	<StrictMode>
-		<App />
+		<header></header>
+		<main>
+			<App />
+		</main>
 	</StrictMode>
 );
